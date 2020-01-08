f12da5a1d62f7b09c47f32e4e79ca39f2f62a3df
Celine Berthelin <celine.berthelin@gmail.com>
2020-01-08 15:20:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const config = {
+  width: 500,
+  height: 300,
+  type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade:{
+      gravity: {y: 450 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+}
+
+const game = new Phaser.Game(config)
+let dude;
+let cursors;
+
+function preload(){
+  this.load.image('dude','./images/bomb.png');
+}
+
+function create(){
+
+  // this.add.image(100, 100, 'dude').setScrollFactor(0);
+  // dude.scale.setTo(0.8,0.8)
+  dude = this.physics.add.image(100, 100, 'dude')
+  dude.body.collideWorldBounds = true;
+
+  cursors = this.input.keyboard.createCursorKeys()
+  console.log(cursors)
+}
+
+function update() {
+  dude.setVelocityX(0)
+
+  if(cursors.up.isDown) {
+    dude.setVelocity(0, -300)
+  } 
+  if(cursors.right.isDown) {
+    dude.setVelocity(300, 0)
+  } 
+  if(cursors.left.isDown) {
+    dude.setVelocity(-300, 0)
+  }
+}
